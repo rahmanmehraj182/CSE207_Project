1f08b706cbbd02ea4349106b6e3a8b58fac44435
rahmanmehraj182 <2022-3-60-182@std.ewubd.edu>
2025-07-24 18:02:37 +0600

    adding faculty apis

--- faculty.h
+++ faculty.h
@@ -1,6 +1,20 @@
 #ifndef FACULTY_H
 #define FACULTY_H
 
+enum designation{
+    LECTURER = 0,
+    SENIOR_LECTURE,
+    ASSISTANT_PROFESSOR,
+    ASSOCIATE_PROFESSOR,
+    PROFESSOR,
+}
 
+typedef struct faculty{
+    char *name;
+    char *id;
+    int designation;
+} faculty;
+
+void add_faculty();
 
 #endif
